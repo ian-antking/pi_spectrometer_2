7e481a6bc3852026bbecabb7864279eec5ef0112
Ian King <ian.antking@gmail.com>
2020-02-17 18:44:57 +0000

    set main window size

--- pi_spectrometer_2/pi_spectrometer.py
+++ pi_spectrometer_2/pi_spectrometer.py
@@ -4,6 +4,8 @@ class App:
     def __init__(self, master):
         self.master = master
         self.master.title('Pi Spectrometer 2')
+        self.master.geometry('1280x200')
+        self.master.resizable(width=False, height=False)
 
 if __name__ == '__main__':
     root = Tk()
